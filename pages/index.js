ccb070a5336221139cdf46b7e53faf02009ad37a
imtu227 <58913392+imtu227@users.noreply.github.com>
2019-12-23 05:10:38 +0800

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        item: [],
+        isLoaded: false
+      }
+    }
+    
+    componentDidMount() {
+
+      fetch('https://github.com/artsy/metaphysics/blob/master/_schema.graphql') //im facing problem with this url
+          .then(res => res.json())
+          .then(json => {
+            this.setState({
+                isLoaded: true,
+                items: json,
+            })
+          });
+    }
+
+
+   render() {
+
+       var { isLoaded, items } = this.state;
+
+       if (!isLoaded) {
+         return <div>Loading...</div>;
+       }
+      
+       else {
+       
+      
+   return (
+    <div className="App">
+        
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} | {item.artists}
+            </li>
+          ))}
+        </ul>
+
+    </div>
+      );
+   }
+}
+
+
+}
+export default App;
+
+const Index = () => (
+    <div>
+      <p>Hello Next.js</p>
+    </div>
+  );
+  
+//  export default Index;
